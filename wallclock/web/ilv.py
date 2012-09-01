14f2dc82ea2b22b942b679e85ef1572178888d20
Michiel Baird <michielbaird@gmail.com>
2012-09-01 07:26:03 -0400

    working get time

--- wallclock/web/ilv.py
+++ wallclock/web/ilv.py
@@ -3,7 +3,7 @@ import socket
 
 from threading import Lock
 from datetime import datetime
-from collections import defualtdict
+from collections import defaultdict
 
 class IdMismatch(Exception):
     pass
@@ -13,18 +13,24 @@ class ErrorMismatch(Exception):
 class ClockUnit:
     GET_TIME_AND_DATE = 0xe5
     SET_TIME_AND_DATE = 0xe6
-    locks = defualtdict(Lock)
+    locks = defaultdict(Lock)
 
     def __init__(self, host_id):
         self.active = False
         self.ip_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host_id  = host_id
 
-    def _buildILV(identifier, value):
-        return struct.pack("BHp", identifier, len(value), value)
+    def _buildILV(self,identifier, value):
+        if value == "":
+            return struct.pack("=BH", identifier, 0)
+        else:
+            return struct.pack("=BHs", identifier, len(value), value)
 
-    def _unpackILV(ilv):
-        i,l,v = struct.unpack("BHp", ilv)
+    def _unpackILV(self, ilv):
+        size = len(ilv) - 3
+        print repr(ilv)
+        i,l = struct.unpack("=BH", ilv[:3])
+        v = ilv[3:]
         return i, v
 
 
@@ -41,13 +47,15 @@ class ClockUnit:
 
 
 
-    def _sendAndReceive(self, ilv, close=True)
+    def _sendAndReceive(self, ilv, close=True):
         self._open()
         start = 0
         while start < len(ilv):
             start += self.ip_socket.send(ilv[start:])
-        ilv_rec = self.ip_socket.recv(2)
-        remaining = ord(ilv[1])
+        ilv_rec = self.ip_socket.recv(3)
+        i , l = struct.unpack("=BH", ilv_rec)
+        print l
+        remaining = l
         ilv_rec += self.ip_socket.recv(remaining)
         if close:
             self._close()
@@ -55,20 +63,23 @@ class ClockUnit:
 
 
     def getDateTime(self):
-        lock.acquire()
-        ilv = self._buildILV(GET_TIME_AND_DATE, "")
+        ClockUnit.locks[self.host_id].acquire()
+        ilv = self._buildILV(ClockUnit.GET_TIME_AND_DATE, "")
+        print repr(ilv)
         result = self._sendAndReceive(ilv)
-        id, value = self._unpackILV
-        if id != GET_TIME_AND_DATE:
+        id, value = self._unpackILV(result)
+        if id != ClockUnit.GET_TIME_AND_DATE:
             raise ID_MISMATCH()
-        error, response = struct.unpack("BP")
+        error = struct.unpack("=B", value[0])[0]
+        response = value[1:]
+        print error, repr(response)
         if error != 0:
             raise ErrorMismatch()
-        dow, year, month, day, hour, minute, second =
-                            struct.unpack("BBBBBBB", response)
+        dow, year, month, day, hour, minute, second = \
+                            struct.unpack("=BBBBBBB", response)
         print  dow, year, month, day, hour, minute, second
 
-        lock.release()
-        return datetime(year, month, day, hour, minute, second)
+        ClockUnit.locks[self.host_id].release()
+        return datetime(year+2000, month, day, hour, minute, second)
 
 
